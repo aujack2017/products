4142b22b8771f8d0bab4c00af1e5e1fa75efd762
AuJack2017 <aujack2017@gmail.com>
2018-10-20 20:08:37 +1000

    Add files

---
+++ products.py
@@ -0,0 +1,16 @@
+products = []
+price = []
+while True:
+    name = input('請輸入產品名稱 ：')
+    if name == 'q':
+    	break
+    price = input('請輸入商品價格：')
+#    p = []
+#    p.append(name)
+#    p.append(price)
+#    p = [name,price]
+    products.append([name,price])
+
+print(products)
+
+print(products[0][0])
