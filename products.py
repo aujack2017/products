f460725112cd9920b8bc1eba30f20eb37686b167
AuJack2017 <aujack2017@gmail.com>
2018-10-22 15:46:50 +1000

    add read file

--- products.py
+++ products.py
@@ -1,30 +1,41 @@
+import csv,codecs
 products = []
-price = []
+
+#讀取檔案，放入products[]
+with codecs.open('products.csv', 'r', encoding='utf-8_sig') as f:
+    for line in f:
+    	if '名稱' in line:
+    		continue	#繼續
+    	name, price = line.strip().split(',')
+    	products.append([name, price])
+print(products)
+
+
+#輸入產品及價錢
 while True:
     name = input('請輸入產品名稱 ：')
     if name == 'q':
     	break
-    price = input('請輸入商品價格：')
-#    p = []
-#    p.append(name)
-#    p.append(price)
-#    p = [name,price]
+    price = int(input('請輸入商品價格：'))
     products.append([name,price])
 
 print(products)
 
-# print(products[0][0])
 
+#寫入檔案
 for p in products:
-	print(p[0] + '的價格是：' + p[1])
+	print(p[0] + '的價格是：' + str(p[1]))
 	# print(p[1])
 
-with open('products.txt','w') as f:
+with codecs.open('products.txt','w',encoding='utf-8_sig') as f:
   	for p in products:
-  	    f.write(p[0] + ',' + p[1] + '\n')
+  	    f.write(p[0] + ',' + str(p[1]) + '\n')
 
-with open('products.csv', 'w', encoding = 'utf-8') as f:
+with codecs.open('products.csv', 'w', encoding='utf-8_sig') as f:
     f.write('名稱' + ',' + '數量' + '\n')
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+
 
